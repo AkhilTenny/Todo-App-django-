b586e59f715d7a4b0a1a697c82aa5b8e62f13891
akhiltenny <akhiltenny10@gmail.com>
2025-03-16 01:11:46 +0530

    added the task to the bd and displayed the tasks in the home.html file. made a decent css style(needs work).

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from myapp.models import TaskModel
 # Register your models here.
+admin.site.register(TaskModel)
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+from .models import TaskModel
 
 # Create your views here.
 
 def home(request):
-  return HttpResponse("Hello django")
 
+  data = TaskModel.objects.all().values()
+  data_dict = {
+    'data':data
+  }
+  return render(request,"home.html",data_dict)
+
+
+
+
+def addTask(request):
+    if request.method == "POST":
+        body = json.loads(request.body.decode('utf-8')) 
+        task = body.get("task")
+        date = body.get('date')
+
+        
+        newDoc = TaskModel(task = task, date = date)
+        
+        newDoc.save()
+
+        return HttpResponse(body)
+        
+        
+        
